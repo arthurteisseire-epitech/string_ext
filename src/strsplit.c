7af8db75f76319d2314268b12c63e49ff2891f9b
arthur <arthur.teisseire@epitech.eu>
2018-10-20 11:24:28 +0200

    Refactor strsplit, now doesn't allocate memory when there is no tokens

--- src/strsplit.c
+++ src/strsplit.c
@@ -10,27 +10,14 @@
 #include <stdbool.h>
 #include "string_ext.h"
 
-static bool is_empty(char **tokens)
-{
-    return (tokens[0] == NULL);
-}
-
-static char **allocate_tokens_pointers(const char *str, const char *delim)
-{
-    size_t nb_tok = strcount(str, delim);
-    char **tokens = malloc(sizeof(char *) * (nb_tok + 1));
-
-    return (tokens);
-}
-
 static char **teardown_string(char **tokens, char *tok_str, const char *delim)
 {
     unsigned int i = 0;
-    char *save_ptr;
+    char *save_ptr = tok_str;
 
-    tokens[0] = strtok_r(tok_str, delim, &save_ptr);
-    while (tokens[i] != NULL)
-        tokens[++i] = strtok_r(NULL, delim, &save_ptr);
+    do
+        tokens[i] = strtok_r(NULL, delim, &save_ptr);
+    while (tokens[i++] != NULL);
     return (tokens);
 }
 
@@ -41,16 +28,15 @@ static char **create_tokens(char **tokens, const char *str, const char *delim)
     if (tok_str == NULL)
         return (NULL);
     tokens = teardown_string(tokens, tok_str, delim);
-    if (is_empty(tokens))
-        free(tok_str);
     return (tokens);
 }
 
 char **strsplit(const char *str, const char *delim)
 {
-    char **tokens = allocate_tokens_pointers(str, delim);
+    size_t nb_tok = strcount(str, delim);
+    char **tokens = calloc(nb_tok + 1, sizeof(char *));
 
-    if (tokens != NULL)
+    if (tokens != NULL && nb_tok != 0)
         tokens = create_tokens(tokens, str, delim);
     return (tokens);
 }
